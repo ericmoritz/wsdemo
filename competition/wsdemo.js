2bdd8f43a9bc7cb1f5dfbff4248b6e4beda0d16f
Eric Moritz <eric@themoritzfamily.com>
2012-05-26 08:43:16 -0400

    Added a node.js implementation of my ticking websocket server

---
+++ competition/wsdemo.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+var WebSocketServer = require("websocket").server;
+var http = require("http");
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(404);
+  response.end();
+});
+
+server.listen(8000, function() {
+  console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+wsServer.on("request", function(request) {
+  var connection = request.accept(null, request.origin);
+
+  function tick() {
+    connection.sendUTF("tick");
+    setTimeout(tick, 1000);
+  }
+
+  setTimeout(tick, 1000);
+  
+  connection.on("message", function(message) {
+    if (message.type === 'utf8') {
+      connection.sendUTF(message.utf8Data);
+    }
+    else if (message.type === 'binary') {
+      connection.sendBytes(message.binaryData);
+    }
+  });
+
+  connection.on('close', function(reasonCode, description) {
+  });
+});
