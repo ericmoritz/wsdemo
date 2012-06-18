42f8b8880988259a8b025370d94a41f8d696a0bb
Eric Moritz <eric@themoritzfamily.com>
2012-06-17 20:59:22 -0400

    Moved this script to bin, but it needs to be ran in the root

---
+++ bin/run_all_tests.sh
@@ -0,0 +1,27 @@
+HOSTNAME=$1
+PORT=$2
+
+do_test() {
+   SERVERNAME=$1
+   echo "Start $SERVERNAME then press enter"
+   read
+
+   echo "Warming up server"
+   ./runtest data/warmup 60 $HOSTNAME $PORT 1000
+
+   echo "Start meminfo on the server then press enter"
+   read
+   echo "Running test for $SERVERNAME"
+   ./runtest data/$SERVERNAME 600 $HOSTNAME $PORT 10000
+}
+
+do_test "erlang-cowboy"
+do_test "ruby-em-websockets"
+do_test "go-websockets"
+do_test "java-webbit"
+do_test "node-websocket"
+do_test "node-ws"
+do_test "perl-ev"
+do_test "haskell-snap"
+do_test "python-tornado"
+do_test "python-ws4py"
