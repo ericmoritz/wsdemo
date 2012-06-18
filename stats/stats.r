7d32e0459947214e6585d532091947a6447bd348
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-06-18 22:56:54 +0200

    Introduce a jitter plot for handshake times.

--- stats/stats.r
+++ stats/stats.r
@@ -10,18 +10,55 @@ base_size <- 9
 
 # Read in data
 counts <- read.csv("counts.csv", header=TRUE)
+handshake <- read.csv("handshake.csv", header=TRUE)
+handshake <- transform(handshake, elapsed_ms = elapsed / 1000)
 
-# First plot, connection timeouts
+# A plot of the connection timeouts as a function
+ws.plot.conn_timeout <- function() {
+    conn_timeouts <- ggplot(counts, aes(x = framework, y = connection_timeouts))
+    (conn_timeouts
+       + geom_bar()
+       + xlab('Framework')
+       + ylab('Connections Lost')
+       + opts(axis.ticks = theme_blank(),
+              axis.text.x = theme_text(size = base_size * 0.8,
+                                       angle = 330,
+                                       hjust = 0,
+                                       colour = "grey50")))
+}
+
+# Volcano plot of the kernel density estimate to compare time elapsed for
+# handshakes
+ws.plot.jitter <- function() {
+    box <- ggplot(handshake, aes(factor(framework), elapsed_ms))
+    (box + geom_jitter(alpha = 0.08)
+       + xlab('Framework')
+       + ylab('Handshake Time (ms)')
+       + opts(axis.ticks = theme_blank(),
+              axis.text.x = theme_text(size = base_size * 0.8,
+                                       angle = 330,
+                                       hjust = 0,
+                                       colour = "grey50")))
+
+#       + stat_density(aes(ymax = ..density.., ymin = -..density..),
+#                      fill = "grey50", colour = "grey50",
+#                      geom = "ribbon", position = "identity")
+#       + facet_grid(. ~ framework) + coord_flip())
+}
+
+## BEGIN PLOTTING
+# Conn timeout
 pdf("stat_results/conn_timeouts.pdf")
-conn_timeouts <- ggplot(counts, aes(x = type, y = connection_timeouts))
-(conn_timeouts
-   + geom_bar()
-   + xlab('Framework')
-   + ylab('Connections Lost')
-   + opts(axis.ticks = theme_blank(),
-          axis.text.x = theme_text(size = base_size * 0.8,
-                                   angle = 330,
-                                   hjust = 0,
-                                   colour = "grey50")))
+ws.plot.conn_timeout()
+dev.off()
+png("stat_results/conn_timeouts.png")
+ws.plot.conn_timeout()
 dev.off()
 
+# Jitter
+pdf("stat_results/handshake_jitter.pdf")
+ws.plot.jitter()
+dev.off()
+png("stat_results/handshake_jitter.png")
+ws.plot.jitter()
+dev.off()
