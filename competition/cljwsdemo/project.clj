8c53355d1dc9030df0e9d01012b4fb70c78dcd6c
r4vi <kotecha.ravi@gmail.com>
2012-06-19 13:36:26 +0200

    add project structure if someone needs to build it again

---
+++ competition/cljwsdemo/project.clj
@@ -0,0 +1,8 @@
+(defproject cljwsdemo "0.1.0-SNAPSHOT"
+  :description "Simple Echo Websocket Server for C10K Webscoket Competition"
+  :url "https://github.com/r4vi/wsdemo"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main cljwsdemo.core          
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [aleph "0.3.0-alpha2"]])
