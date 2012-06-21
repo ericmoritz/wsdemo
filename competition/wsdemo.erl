0079dba82a04d6f6e10eaeb59d7f9b1f338c1282
Eric Moritz <eric@themoritzfamily.com>
2012-06-20 21:17:34 -0400

    Moved the erlang wsdemo server to competition.  src/ is now just the tool

---
+++ competition/wsdemo.erl
@@ -0,0 +1,35 @@
+-module(wsdemo).
+
+-export([start/1, start/0]).
+
+-export([init/3, websocket_init/3, websocket_handle/3, websocket_info/3,
+         websocket_terminate/3]).
+
+start() ->
+    start(8000).
+
+start(Port) ->
+    application:start(cowboy),
+
+    Dispatch = [
+                {'_', [{'_', ?MODULE, []}]}
+               ],
+
+    cowboy:start_listener(?MODULE, 100,
+                          cowboy_tcp_transport, [{port, Port}, {max_connections, infinity}],
+                          cowboy_http_protocol, [{dispatch, Dispatch}]).
+    
+init({tcp, http}, _Req, _Opts) ->
+    {upgrade, protocol, cowboy_http_websocket}.
+
+websocket_init(_TransportName, Req, State) ->
+    {ok, Req, State, hibernate}.
+
+websocket_handle(Msg, Req, State) ->
+    {reply, Msg, Req, State, hibernate}.
+
+websocket_info(_Msg, Req, State) ->
+    {ok, Req, State, hibernate}.
+
+websocket_terminate(_Reason, _Req, _State) ->
+    ok.
