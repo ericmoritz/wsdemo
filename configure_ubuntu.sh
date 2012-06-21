68c346417b9f9f88b75e6c38bd557fe88212a174
ericmoritz <eric@themoritzfamily.com>
2012-06-20 21:55:26 -0300

    Added pypy and made node only download once.

--- configure_ubuntu.sh
+++ configure_ubuntu.sh
@@ -1,21 +1,38 @@
-sudo apt-get install -y emacs python-dev python-setuptools build-essential erlang-nox libevent-dev git golang mercurial default-jdk ruby rubygems haskell-platform
-sudo easy_install ws4py gevent
+#!/bin/bash
+# Note: pushd/popd only work in bash
 
+sudo apt-get install -y curl emacs python-dev python-setuptools build-essential erlang-nox libevent-dev git golang mercurial default-jdk ruby rubygems haskell-platform
+
+# Clone master
 git clone git://github.com/ericmoritz/wsdemo.git wsdemo
 
+# Update sysctl
 sudo cp wsdemo/etc/sysctl.conf /etc/
 sudo sysctl -p
 
-mkdir src
-pushd src
-  curl http://nodejs.org/dist/v0.7.11/node-v0.7.11.tar.gz | tar xz
-  pushd node-v0.7.11
-    ./configure && make && sudo make install
-  popd
+# install pypy
+pushd wsdemo/competition
+    if [ ! -d ./pypy-1.9 ]; then
+       curl http://cdn.bitbucket.org/pypy/pypy/downloads/pypy-1.9-linux64.tar.bz2 | tar xj
+    fi
+    curl http://python-distribute.org/distribute_setup.py | ./pypy-1.9/bin/pypy
+    ./pypy-1.9/bin/easy_install pip
+    ./pypy-1.9/bin/pip install tornado ws4py
 popd
 
-npm install websocket
-npm install ws
+
+# install Node
+mkdir src
+pushd src
+  if [ ! -d node-v0.7.11 ]; then
+     curl http://nodejs.org/dist/v0.7.11/node-v0.7.11.tar.gz | tar xz
+     pushd node-v0.7.11
+       ./configure && make && sudo make install
+     popd
+  fi
+popd
+
+npm install websocket ws
 sudo go get code.google.com/p/go.net/websocket
 sudo gem install em-websocket
 
@@ -27,4 +44,3 @@ echo -e "y\ny\no conf prerequisites_policy follow\no conf commit" | sudo cpan
 sudo cpan Protocol::WebSocket
 sudo cpan EV::ADNS
 yes | sudo cpan IO::Stream
-
