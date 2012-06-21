fffbc94b0dc038b781e7dd2b94a9ae3e16e158c3
Alexander Glyzov <bonoba@gmail.com>
2012-06-22 00:18:49 +0200

    adds a misc prefork.py module
    to easily fork out N worker processes

---
+++ competition/prefork.py
@@ -0,0 +1,17 @@
+from multiprocessing import cpu_count
+from os              import fork
+
+def prefork( num=None ):
+    """ Forks current process num times.
+        Returns a list of child pids for master or an empty list for a child.
+    """
+    if not num:
+        num = cpu_count()
+    pids = []
+    for n in range(num):
+        pid = fork()
+        if pid:
+            pids.append( pid )
+        else:
+            break
+    return pids
