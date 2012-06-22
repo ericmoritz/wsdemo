37ae3d49cab6e362156224eba906ec751db1c6a0
Eric Moritz <eric@themoritzfamily.com>
2012-06-21 21:38:49 -0400

    Used the new test runner

--- bin/run_all_tests.sh
+++ bin/run_all_tests.sh
@@ -1,28 +1,7 @@
-HOSTNAME=$1
-PORT=$2
+if [ "x$1" == "x" -o "x$2" == "x" ]; then 
+    echo "Usage: $0 Name Config"
+    exit
+fi
 
-do_test() {
-   SERVERNAME=$1
-   echo "Start $SERVERNAME then press enter"
-   read
-
-   echo "Warming up server"
-   ./runtest data/warmup 60 $HOSTNAME $PORT 1000
-
-   echo "Start ./bin/meminfo on the server then press enter"
-   read
-   echo "Running test for $SERVERNAME"
-   ./runtest data/$SERVERNAME 600 $HOSTNAME $PORT 10000
-}
-
-do_test "erlang-cowboy"
-do_test "go-websockets"
-do_test "haskell-snap"
-do_test "java-webbit"
-do_test "node-websocket"
-do_test "node-ws-cluster"
-do_test "perl-ev"
-do_test "python-tornado"
-do_test "python-ws4py"
-do_test "ruby-em-websockets"
+erl -boot start_sasl -pa ebin deps/*/ebin -s wsdemo_bench -name "$1" -config "$2" -eval "wsdemo_bench:run_sync(), init:stop()."
 
