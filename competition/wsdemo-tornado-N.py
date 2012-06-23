6901c84174a591f1b1d751ca9eca40ee6a0e6349
Alexander Glyzov <bonoba@gmail.com>
2012-06-23 22:35:55 +0000

    fixes python forking, tweaks python servers

--- competition/wsdemo-tornado-N.py
+++ competition/wsdemo-tornado-N.py
@@ -3,6 +3,7 @@ from tornado.web        import Application
 from tornado.websocket  import WebSocketHandler
 from tornado.httpserver import HTTPServer
 from tornado.netutil    import bind_sockets
+from logging            import getLogger, CRITICAL
 from prefork            import prefork, cpu_count
 
 class EchoApp( Application ):
@@ -13,19 +14,18 @@ class WSHandler( WebSocketHandler ):
     def allow_draft76( self ):
         return False
 
-    def open( self ):
-        pass
-
     def on_message( self, message ):
         self.write_message( message, binary=True )
 
 if __name__ == "__main__":
+    getLogger().setLevel( CRITICAL )
+
     server  = HTTPServer( EchoApp() )
-    sockets = bind_sockets( 8000 )
+    sockets = bind_sockets( 8000, backlog=768 )
 
     num = cpu_count()
-    print "Tornado (%s workers)" % (num+1)
-    prefork( num )
+    print "Tornado (%s workers)" % num
+    if prefork( num ): exit()
 
     server.add_sockets( sockets )
     IOLoop.instance().start()
