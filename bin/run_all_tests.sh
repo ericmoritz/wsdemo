62534c6891cdb17f7d00d979b409f463a39493c8
Eric Moritz <eric@themoritzfamily.com>
2012-06-23 22:34:27 -0400

    Removed the name argument.
    Now that I'm using supervisord's XML-RPC interface, I no longer need to
    use distirbuted Erlang :(.  It was fun while it lasted.

--- bin/run_all_tests.sh
+++ bin/run_all_tests.sh
@@ -1,7 +1,7 @@
-if [ "x$1" == "x" -o "x$2" == "x" ]; then 
-    echo "Usage: $0 Name Config"
+if [ "x$1" == "x" ]; then 
+    echo "Usage: $0 Config"
     exit
 fi
 
-erl -boot start_sasl -pa ebin deps/*/ebin -s wsdemo_bench -name "$1" -config "$2" -eval "wsdemo_bench:run_sync(), init:stop()."
+erl -boot start_sasl -pa ebin deps/*/ebin -s wsdemo_bench -config "$1" -eval "wsdemo_bench:run_sync(), init:stop()."
 
